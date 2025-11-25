512ce05f0debd52acd71eaee8ca863612d1836f5
Douglas Cheah <10919284+douglascgh@users.noreply.github.com>
2025-11-25 14:11:48 -0800

    [Rust][POC]Add basic temperature sensor rust wrapper

--- zephyr-sys/build.rs
+++ zephyr-sys/build.rs
@@ -78,6 +78,7 @@ fn main() -> Result<()> {
         .allowlist_function("k_.*")
         .allowlist_function("gpio_.*")
         .allowlist_function("flash_.*")
+        .allowlist_function("sensor_.*")
         .allowlist_function("zr_.*")
         .allowlist_item("GPIO_.*")
         .allowlist_item("FLASH_.*")
