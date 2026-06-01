14baa3001300337b44c59bf73f73be82abff2ae9
bjackson312006 <blakejackson312006@gmail.com>
2026-06-01 13:32:21 -0700

    stop tracking build/

--- docgen/build/zephyr/NSI/common/src/nsi_trace_varg.d
+++
@@ -1,6 +0,0 @@
-/home/blake/Repos/zephyr/zephyr-rust/modules/lang/rust/docgen/build/zephyr/NSI/common/src/nsi_trace_varg.o: \
- /home/blake/Repos/zephyr/zephyr-rust/zephyr/scripts/native_simulator//common/src/nsi_trace_varg.c \
- /home/blake/Repos/zephyr/zephyr-rust/zephyr/scripts/native_simulator/common/src/include/nsi_tracing.h \
- /home/blake/Repos/zephyr/zephyr-rust/zephyr/scripts/native_simulator/common/src/include/nsi_utils.h
-/home/blake/Repos/zephyr/zephyr-rust/zephyr/scripts/native_simulator/common/src/include/nsi_tracing.h:
-/home/blake/Repos/zephyr/zephyr-rust/zephyr/scripts/native_simulator/common/src/include/nsi_utils.h:
