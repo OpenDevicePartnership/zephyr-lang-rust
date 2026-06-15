1767edcacb783bd6c072589dd5a259b782fdd14a
bjackson312006 <blakejackson312006@gmail.com>
2026-06-15 15:14:34 -0700

    builds with sample

---
+++ samples/thermal-service/src/battery.rs
@@ -0,0 +1,88 @@
+use embedded_services::{error, info};
+use static_cell::StaticCell;
+
+pub type BatteryService = battery_service::Service<'static, 1>;
+
+const BAT_ID: battery_service::device::DeviceId = battery_service::device::DeviceId(0);
+
+pub async fn init(spawner: embassy_executor::Spawner) -> BatteryService {
+    info!("Initializing battery service...");
+
+    static BATTERY_DEVICE: StaticCell<battery_service::device::Device> = StaticCell::new();
+    let device = BATTERY_DEVICE.init(battery_service::device::Device::new(BAT_ID));
+    let driver = zephyr::devicetree::labels::fuel_gauge::get_instance().expect("Failed to call get_instance() for fuel_gauge.");
+    let battery = battery_service::wrapper::Wrapper::new(device, driver);
+
+    let service = crate::utils::spawn_service!(spawner, BatteryService, |resources| battery_service::Service::new(
+        resources,
+        battery_service::InitParams {
+            devices: [device],
+            config: battery_service::context::Config::default(),
+        }
+    ))
+    .expect("Failed to initialize battery service");
+
+    spawner.spawn(battery_device_controller_task(battery)).expect("Failed to spawn battery device controller task");
+    spawner.spawn(update_data_task(service)).expect("Failed to spawn battery update data task");
+
+    service
+}
+
+#[embassy_executor::task]
+async fn battery_device_controller_task(battery: battery_service::wrapper::Wrapper<'static, zephyr::device::fuel_gauge::FuelGauge>) {
+    battery.process().await;
+}
+
+#[embassy_executor::task]
+pub async fn update_data_task(service: BatteryService) -> ! {
+
+    // Helper to tell the statemachine to try to recover.
+    async fn recover_state_machine(service: &BatteryService) -> Result<(), ()> {
+        loop {
+            match service.execute_event(battery_service::context::BatteryEvent {event: battery_service::context::BatteryEventInner::Timeout, device_id: BAT_ID}).await {
+                Ok(_) => return Ok(()),
+                Err(battery_service::context::ContextError::StateError(battery_service::context::StateMachineError::DeviceTimeout)) => {embassy_time::Timer::after_secs(10).await;}
+                Err(battery_service::context::ContextError::StateError(battery_service::context::StateMachineError::NoOpRecoveryFailed)) => return Err(()),
+                _ => {}
+            }
+        }
+    }
+
+    let mut failures: u32 = 0;
+    let mut count: usize = 0;
+    loop {
+        embassy_time::Timer::after_secs(1).await;
+        if count.is_multiple_of(const { 60 * 60 })
+            && let Err(e) = service
+                .execute_event(battery_service::context::BatteryEvent {
+                    event: battery_service::context::BatteryEventInner::PollStaticData,
+                    device_id: BAT_ID,
+                })
+                .await
+        {
+            failures += 1;
+            error!("FG: Static data error: {:#?}", e);
+        }
+        if let Err(e) = service
+            .execute_event(battery_service::context::BatteryEvent {
+                event: battery_service::context::BatteryEventInner::PollDynamicData,
+                device_id: BAT_ID,
+            })
+            .await
+        {
+            failures += 1;
+            error!("FG: Dynamic data error: {:#?}", e);
+        }
+
+        if failures > 10 {
+            failures = 0;
+            count = 0;
+            error!("FG: Too many errors, timing out and starting recovery...");
+            if recover_state_machine(&service).await.is_err() {
+                error!("FG: Failed to recover state machine!");
+            }
+        }
+
+        count = count.wrapping_add(1);
+    }
+}
